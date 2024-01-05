f10270c93e75e4ede57cbf5dd33da9a06812a5a6
John <tungusv89@gmail.com>
2024-01-05 12:38:54 +0300

    каккой то баг с блоком кейсов

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', () => {
+// 	document.querySelectorAll('.btn-gph').forEach(button => {
+// 		button.addEventListener('click', () => {
+// 			button.data
+// 			document.querySelector('.case').classList.add('active');
+// 		});
+// 	});
+// });
