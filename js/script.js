05aa50c7e9a093b843d785bfd200dea1a1f444b5
John <tungusv89@gmail.com>
2024-01-08 12:51:27 +0300

    modal window

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-// document.addEventListener('DOMContentLoaded', () => {
-// 	document.querySelectorAll('.btn-gph').forEach(button => {
-// 		button.addEventListener('click', () => {
-// 			button.data
-// 			document.querySelector('.case').classList.add('active');
-// 		});
-// 	});
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.btn-gph').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      document.querySelector(`#${e.target.dataset.target}`).classList.add('active');
+    });
+
+    document.querySelector(`#${button.dataset.target}`).addEventListener('click', (e) => {
+      e.target.classList.remove('active');
+    });
+  });
+});
