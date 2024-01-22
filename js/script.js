bb4f2cf4c506bafaa489db79f6e3978da7e060b5
John <tungusv89@gmail.com>
2024-01-22 19:10:38 +0300

    1 экран готов

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Открытие/закрытие модального окна
   document.querySelectorAll('.btn-gph').forEach((button) => {
     button.addEventListener('click', (e) => {
       document.querySelector(`#${e.target.dataset.target}`).classList.add('active');
@@ -8,4 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
       e.target.classList.remove('active');
     });
   });
+
+  // Меню при скролле
+  const header = document.querySelector('header');
+  const headerMobile = header.cloneNode(true);
+
+  header.before(headerMobile);
+  headerMobile.classList.add('header-mobile');
+  headerMobile.style.top = `-${header.clientHeight}px`;
+  window.onscroll = function () {
+    const margin = document.querySelector('.main-screen').clientHeight / 2;
+    // debugger;
+    if (document.body.scrollTop > margin || document.documentElement.scrollTop > margin) {
+      headerMobile.style.top = '0';
+    } else {
+      headerMobile.style.top = `-${header.clientHeight}px`;
+    }
+  };
 });
